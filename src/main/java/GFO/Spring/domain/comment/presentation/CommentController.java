86e1c2b467ae4788f856a5addb78a52ea9d4206c
KimTaeO <taeo0328@gmail.com>
2023-03-14 00:17:34 +0900

    :sparkles: 댓글 삭제 controller 작성

--- src/main/java/GFO/Spring/domain/comment/presentation/CommentController.java
+++ src/main/java/GFO/Spring/domain/comment/presentation/CommentController.java
@@ -3,6 +3,7 @@ package GFO.Spring.domain.comment.presentation;
 import GFO.Spring.domain.comment.presentation.dto.request.CreateCommentReqDto;
 import GFO.Spring.domain.comment.presentation.dto.request.ModifyCommentReqDto;
 import GFO.Spring.domain.comment.service.CreateCommentService;
+import GFO.Spring.domain.comment.service.DeleteCommentService;
 import GFO.Spring.domain.comment.service.ModifyCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import javax.validation.Valid;
 public class CommentController {
     private final CreateCommentService createCommentService;
     private final ModifyCommentService modifyCommentService;
+    private final DeleteCommentService deleteCommentService;
 
     @PostMapping("/{id}")
     public ResponseEntity<Void> createComment(@PathVariable Long id, @RequestBody @Valid CreateCommentReqDto createCommentReqDto) {
@@ -27,6 +29,12 @@ public class CommentController {
     @PatchMapping("/{id}")
     public ResponseEntity<Void> modifyComment(@PathVariable Long id, @RequestBody @Valid ModifyCommentReqDto modifyCommentReqDto) {
         modifyCommentService.execute(id, modifyCommentReqDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        deleteCommentService.execute(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
